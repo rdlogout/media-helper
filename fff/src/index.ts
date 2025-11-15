31c07a858b2da486b574df53575e84b7df02e891
rahulkumar <base15log@gmail.com>
2025-11-15 21:20:31 +0530

    sharp added

--- fff/src/index.ts
+++ fff/src/index.ts
@@ -1,15 +1,32 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import sharp from "sharp";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/image", async (c) => {
+	const width = c.req.query("width") || 200;
+	const height = c.req.query("height") || 200;
+	const imageUrl = c.req.query("url") || "https://i.ytimg.com/vi/xtJA_3kH4Qg/hqdefault.jpg";
+	const stream = await fetch(imageUrl).then((res) => res.arrayBuffer());
+	const resizedStream = await sharp(Buffer.from(stream)).resize(Number(width), Number(height)).toBuffer();
+	return new Response(resizedStream, {
+		headers: {
+			"Content-Type": "image/jpeg",
+		},
+	});
+});
+
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	}
+);
