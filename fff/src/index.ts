33e403159a3ebce68a15abcb6031f5311ed7bf56
rahulkumar <base15log@gmail.com>
2025-11-15 21:35:06 +0530

    sync

--- fff/src/index.ts
+++ fff/src/index.ts
@@ -12,13 +12,29 @@ app.get("/image", async (c) => {
 	const width = c.req.query("width") || 200;
 	const height = c.req.query("height") || 200;
 	const imageUrl = c.req.query("url") || "https://i.ytimg.com/vi/xtJA_3kH4Qg/hqdefault.jpg";
+
+	const cacheUrl = c.req.url;
+	const cacheKey = new Request(cacheUrl.toString());
+	const cache = caches.default;
+	const cacheResponse = await cache.match(cacheKey);
+
+	if (cacheResponse) {
+		return new Response(cacheResponse.body, {
+			headers: cacheResponse.headers,
+			status: 201,
+		});
+	}
+
 	const stream = await fetch(imageUrl).then((res) => res.arrayBuffer());
 	const resizedStream = (await sharp(Buffer.from(stream)).resize(Number(width), Number(height)).toBuffer()) as any;
-	return new Response(resizedStream, {
+	const response = new Response(resizedStream, {
 		headers: {
 			"Content-Type": "image/jpeg",
 		},
 	});
+	c.executionCtx.waitUntil(cache.put(cacheKey, response.clone()));
+
+	return response;
 });
 
 serve(
