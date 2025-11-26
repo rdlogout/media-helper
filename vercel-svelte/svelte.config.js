acbcd93fda7f0e70d9d17d7eafeeb5e81864e9fd
rahulkumar <base15log@gmail.com>
2025-11-26 10:31:41 +0530

    sync

--- vercel-svelte/svelte.config.js
+++ vercel-svelte/svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		paths: {
-			assets: `https://m1.fussion.studio`,
+			assets: `https://media-vercel.b-cdn.net`,
 		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
